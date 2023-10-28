975811a1845348d5ebec2b11d4fbcc68f5c02fda
malushkru0878 <malushkru08@mail.ru>
2023-10-28 19:20:51 +0300

    HW arrays added

---
+++ 2.functions/task.js
@@ -0,0 +1,44 @@
+// Задание 1
+function getArrayParams(arr) {
+  let min, max, sum, avg;
+  min = Infinity;
+  max = - Infinity;
+  sum = 0;
+
+  for (let number of arr) {
+    if (number < min) {
+      min = number
+    }
+    if (number > max) {
+      max = number;
+    }
+    sum += number;
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };
+}
+
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let number of arr) {
+    sum += number;
+  }
+  return sum;
+}
+
+function makeWork(arrOfArr, func) {
+  let max = - Infinity;
+  for (let arr of arrOfArr) {
+    if (func(arr) > max) {
+      max = func(arr)
+    }
+  }
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  return getArrayParams(arr).max - getArrayParams(arr).min;
+}
