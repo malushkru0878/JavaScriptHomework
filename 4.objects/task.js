39123e1e6c867c9985ee52a7ed8bc687d1adbad2
malushkru0878 <malushkru08@mail.ru>
2023-10-29 10:27:44 +0300

    Update README.md

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,28 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...marks) {
+  if (this.hasOwnProperty("marks")){
+    this.marks.push(...marks);
+ }
+}
+
+Student.prototype.getAverage = function () {
+    if (this.hasOwnProperty("marks") && this.marks != []) {
+      return this.marks.reduce( (accumulator, current, index, array) => accumulator + current/array.length, 0)
+ } else return 0;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete(this.marks);
+  delete(this.subject);
+  this.excluded = reason;
+}
